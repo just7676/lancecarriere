559d52b2543642b98263541d36f7f86adfae19f6
Justin Bashige <bashigejustin0@gmail.com>
2025-08-13 21:45:17 +0000

    REMPLACE CE BOUTON PAR JUSTIN_BASHIGE2025/GDGOCUCB/BUILD_WITH_AI_DEMO

--- src/components/app/main-layout.tsx
+++ src/components/app/main-layout.tsx
@@ -108,7 +108,7 @@ export function MainLayout() {
            <Button variant="outline" className="justify-start gap-2 group-data-[collapsible=icon]:h-9 group-data-[collapsible=icon]:w-9 group-data-[collapsible=icon]:justify-center group-data-[collapsible=icon]:p-0" asChild>
                 <a href="https://github.com/JUSTIN_BASHIGE2025/GDGOCUCB/BUILD_WITH_AI_DEMO" target="_blank" rel="noopener noreferrer">
                     <Github className="h-5 w-5" />
-                    <span className="group-data-[collapsible=icon]:hidden text-xs truncate">Code Source</span>
+                    <span className="group-data-[collapsible=icon]:hidden text-xs truncate">JUSTIN_BASHIGE2025/GDGOCUCB/BUILD_WITH_AI_DEMO</span>
                 </a>
             </Button>
         </SidebarFooter>
